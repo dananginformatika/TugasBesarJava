5a6de502d7dee7c1e711b93f8acb771a4aa59592
dananginformatika <danangboll@gmail.com>
2015-05-21 09:41:30 +0700

    Update kondisikelas.java

--- src/tugasbesar/kondisikelas.java
+++ src/tugasbesar/kondisikelas.java
@@ -10,182 +10,94 @@ import java.util.Scanner;
  *
  * @author Danang
  */
-public class kondisikelas extends RuangKelas{
-    Scanner kn = new Scanner(System.in);
-    
-    private String namaruangan;
-    private String lokasiruaangan;
-    private String programstudi;
-    private String fakultas;
-    private int panjang;
-    private int lebar;
-    private int jumlahkursi;
-    private int jumlahpintu;
-    private int jumlahjendela;
-    private double luas;
-    private double rasioluas;
+public class kebersihanKelas extends RuangKelas{
+    Scanner as = new Scanner(System.in);
     
     
+    private String sirkulasiudara;
+    private String pencahayaan;
+    private String kelembaban;
+    private String suhu;
 
-    double hitungluas(int panjang, int lebar){
-        luas = panjang*lebar;
-        return luas;
-    }
+    Scanner baru=new Scanner(System.in);
     
-    String bentuk (int panjang , int lebar){
-        
-        if (panjang!=lebar){
-        return "sesuai";
-        }
-        else {
-        return "tidak sesuai";
-        }
-    }
-    
-    String hitungrasio (int panjang, int lebar){
-        int rasioluas = (panjang*lebar) / jumlahkursi;
-                if(rasioluas >= 0.5){
-                return "sesuai";
-                } 
-                else {
-                return "tidak sesuai";
-                }
-    }
-    
-    String jumlahpintujadi (int jumlahpintu,int jumlahjendela){
-    if (jumlahpintu>=2 && jumlahjendela >=1){
-    return "sesuai";
-    }
-    else {
-        return "tidak sesuai";
-    }
-    
-    
-    }
-    
-    public String getNamaruangan() {
-        return namaruangan;
+    public String getSirkulasiudara() {
+        return sirkulasiudara;
     }
 
-    public void setNamaruangan(String namaruangan) {
-        this.namaruangan = namaruangan;
+    public void setSirkulasiudara(String sirkulasiudara) {
+        this.sirkulasiudara = sirkulasiudara;
+        if("lancar".equals(sirkulasiudara) || "Lancar".equals(sirkulasiudara))
+            this.sirkulasiudara="sesuai";
+        else
+            this.sirkulasiudara="tidak sesuai";
+            }
+
+    public String getPencahayaan() {
+        return pencahayaan;
     }
 
-    public String getLokasiruaangan() {
-        return lokasiruaangan;
+    public void setPencahayaan(String pencahayaan) {
+        this.pencahayaan = pencahayaan;
+        if("terang".equals(pencahayaan) || "Terang".equals(pencahayaan))
+            this.pencahayaan="sesuai";
+        else
+            this.pencahayaan="tidak sesuai";
     }
 
-    public void setLokasiruaangan(String lokasiruaangan) {
-        this.lokasiruaangan = lokasiruaangan;
+    public String getKelembaban() {
+        return kelembaban;
     }
 
-    public String getProgramstudi() {
-        return programstudi;
+    public void setKelembaban(String kelembaban) {
+        this.kelembaban = kelembaban;
+        if("Tidak lembab".equals(kelembaban) || "tidak lembab".equals(kelembaban))
+            this.kelembaban="sesuai";
+        else if("lembab".equals(kelembaban) || "Lembab".equals(kelembaban))
+            this.kelembaban="tidak sesuai";
+        else
+            this.kelembaban="inputan salah";
     }
 
-    public void setProgramstudi(String programstudi) {
-        this.programstudi = programstudi;
+    public String getSuhu() {
+        return suhu;
     }
 
-    public String getFakultas() {
-        return fakultas;
-    }
-
-    public void setFakultas(String fakultas) {
-        this.fakultas = fakultas;
-    }
-
-    public int getPanjang() {
-        return panjang;
-    }
-
-    public void setPanjang(int panjang) {
-        this.panjang = panjang;
-    }
-
-    public int getLebar() {
-        return lebar;
-    }
-
-    public void setLebar(int lebar) {
-        this.lebar = lebar;
-    }
-
-    public int getJumlahkursi() {
-        return jumlahkursi;
-    }
-
-    public void setJumlahkursi(int jumlahkursi) {
-        this.jumlahkursi = jumlahkursi;
-    }
-
-    public int getJumlahpintu() {
-        return jumlahpintu;
-    }
-
-    public void setJumlahpintu(int jumlahpintu) {
-        this.jumlahpintu = jumlahpintu;
-    }
-
-    public int getJumlahjendela() {
-        return jumlahjendela;
-    }
-
-    public void setJumlahjendela(int jumlahjendela) {
-        this.jumlahjendela = jumlahjendela;
-    }
-
-    public double getLuas() {
-        return luas;
-    }
-
-    public void setLuas(double luas) {
-        this.luas = luas;
+    public void setSuhu(String suhu) {
+        this.suhu = suhu;
+        if("sejuk".equals(suhu) || "Sejuk".equals(suhu))
+            this.suhu="sesuai";
+        else if("Panas".equals(suhu) || "panas".equals(suhu))
+            this.suhu="tidak sesuai";
+        else
+            this.suhu="inputan salah";
     }
     
     
     
-    public void kondisi(){
+    
+    
+    public void kebersihan(){
     //public void inputan(){
-        System.out.println("Nama ruang = ");
-        String da=kn.next();
-        setNamaruangan(da);
-        System.out.println("Lokasi ruang = ");
-        String dd=kn.next();
-        setLokasiruaangan(dd);
-        System.out.println("Program studi = ");
-        String db=kn.next();
-        setProgramstudi(db);
-        System.out.println("Fakultas = ");
-        String dc=kn.next();
-        setFakultas(dc);
-        System.out.println("\nPanjang ruang = ");
-        int de=kn.nextInt();
-        setPanjang(de);
-        System.out.println("Lebar ruang = ");
-         int df=kn.nextInt();
-        setLebar(df);
-        System.out.println("Jumlah kursi = ");
-         int dg=kn.nextInt();
-        setJumlahkursi(dg);
-        System.out.println("jumlah pintu = ");
-         int dh=kn.nextInt();
-        setJumlahpintu(dh);
-        System.out.println("Jumlah jendela = ");
-         int di=kn.nextInt();
-        setJumlahjendela(di);
-        
+        System.out.println("\nSirkulasi udara (lancar/tidak) = ");
+        String ab=as.next();
+        setSirkulasiudara(ab);
+        System.out.println("Pencahayaan (terang/tidak) = ");
+        String ac=as.next();
+        setPencahayaan(ac);
+        System.out.println("Kelembaban (lembab/tidak) = ");
+        String ad=as.next();
+        setKelembaban(ad);
+        System.out.println("Suhu (sejuk/panas) = ");
+        String ae=as.next();
+        setSuhu(ae);
     }
     
-    public void hasilkondisi(){
+    public void hasilkebersihan(){
     //public void outputan(){
-        System.out.println("Ruangan "+getNamaruangan());
-        System.out.println(getLokasiruaangan());
-        System.out.println("Jurusan "+getProgramstudi());
-        System.out.println("fakultas "+getFakultas());
-        System.out.println("\nPanjang ruang "+getPanjang());
-        System.out.println("Lebar ruang "+getLebar());
-        System.out.println("Kesesuaian ruangan = ");
+        System.out.println("\nSirkulasi udara = "+getSirkulasiudara());
+        System.out.println("Pencahayaan = "+getPencahayaan());
+        System.out.println("Kelembaban = "+getKelembaban());
+        System.out.println("Suhu = "+getSuhu());
     }
-    
 }
